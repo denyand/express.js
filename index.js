e3e80a9ca867df069d51ea1aeb64eb00195f1bd5
denyand <denyandrian2002@gmail.com>
2023-09-26 18:01:26 +0700

    update hari ni

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-//buat import package express yang sudah di install
 const express = require('express');
 //buat instance express
+
+//buat baca db fake
+const fs = require('fs')
 const app = express();
 const PORT = 3000;
 
+
+app.use(express.static('public'))
 app.get("/" , (req, res) => {
-    res.send("<h1>Home Page Deny</h1>");
+
 }) 
 
+app.get('/users', (req, res) => {
+//task menerima request dari user, ke endpoint '/users harapan nya dari api yang dibuat akan meberikan response berupa data user, untuk kali ini daimbil dri fake_database.json
+    fs.readFile('./db/fake_database.json', 'utf-8',(error, data) => {
+    if (error) res.send("terjadi kesalahan pada pembacaan file")
+    //if (error) throw error
+    res.send(JSON.parse(data))
+    })
+    //jika nnti mau ngirim database real maka perlu melakukan query ke database, misalnya kalau di mySql atau postgree yaitu dengan SELECT * FROM USERS
+})
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
