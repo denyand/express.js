202082cfb93128cc8b2d4f5cb483fb02b54bff6c
denyand <denyandrian2002@gmail.com>
2023-09-26 14:14:29 +0700

    update lagi

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+//buat import package express yang sudah di install
 const express = require('express');
+//buat instance express
 const app = express();
 const PORT = 3000;
 
 app.get("/" , (req, res) => {
-    
     res.send("<h1>Home Page</h1>");
 }) 
 
